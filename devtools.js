f0321ed90ff36b4c207a034354a8e640467a435a
Forrest Chang <fchang@hedgeye.com>
2016-07-01 17:54:41 -0700

    only make panel if Opal is defined

--- devtools.js
+++ devtools.js
@@ -1,8 +1,36 @@
-chrome.devtools.panels.create(
-    "Opal-Console",
-    "assets/images/logo_128.png",
-    "opal-console.html",
-    function cb(panel) {
-        panel.onShown.addListener(function(win){ win.focus(); });
+var panelCreated = false;
+
+function createPanelIfOpalLoaded() {
+    if (panelCreated) {
+        return;
     }
-);
+    chrome.devtools.inspectedWindow.eval(`!! window.Opal`, function(pageHasOpal, err) {
+        if (!pageHasOpal || panelCreated) {
+            return;
+        }
+
+        clearInterval(loadCheckInterval);
+        panelCreated = true;
+        chrome.devtools.panels.create(
+            "Opal-Console",
+            "assets/images/logo_128.png",
+            "opal-console.html",
+            function cb(panel) {
+                panel.onShown.addListener(function(win){ win.focus(); });
+            }
+        );
+    });
+}
+
+chrome.devtools.network.onNavigated.addListener(function() {
+    createPanelIfOpalLoaded();
+});
+
+// Check to see if Opal has loaded once per second in case Opal is added
+// after page load
+var loadCheckInterval = setInterval(function() {
+    createPanelIfOpalLoaded();
+}, 1000);
+
+createPanelIfOpalLoaded();
+
